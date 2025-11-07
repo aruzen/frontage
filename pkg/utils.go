05ffcc2558ed656ffe1a6916b860debe2b9944a3
aruzen <aruzenueii@gmail.com>
2025-11-07 18:13:21 +0900

    add: Localize

--- pkg/utils.go
+++ pkg/utils.go
@@ -10,12 +10,34 @@ type Point struct {
 	Y int
 }
 
+type LocalizeTag string
+
+type Localized interface {
+	Name() LocalizeTag
+	Description() LocalizeTag
+	Summery() []Localized
+}
+
+func SizeToPoint(s Size) Point {
+	return Point{
+		X: s.Width,
+		Y: s.Height,
+	}
+}
+
+func PointToSize(s Point) Size {
+	return Size{
+		Width:  s.X,
+		Height: s.Y,
+	}
+}
+
 func Make2D[T interface{}](size Size, init T) [][]T {
 	data := make([]T, size.Height*size.Width)
 	grid := make([][]T, size.Width)
 	for i := range grid {
 		grid[i] = data[i*size.Width : (i+1)*size.Width]
-		for j, _ := range grid[i] {
+		for j := range grid[i] {
 			grid[i][j] = init
 		}
 	}
